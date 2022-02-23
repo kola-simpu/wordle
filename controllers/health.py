46d64024686ef4cdf579816886f5965a2517e60a
kolawolebalogun <kola@simpu.co>
2022-02-23 16:20:46 -0500

    [DEBUG]

--- controllers/health.py
+++ controllers/health.py
@@ -1,6 +1,6 @@
 from flask import jsonify
-
+import os
 
 def index():
-    data = {"message": "Welcome to wordle game"}
+    data = {"message": "Welcome to wordle game {}".format(os.getenv("FLASK_ENV"))}
     return jsonify(data), 200
