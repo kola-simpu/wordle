a1e7efaa329fed33a80ea55342d536593b3c21f1
kolawolebalogun <kola@simpu.co>
2022-02-23 17:05:24 -0500

    Game CRUD

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from controllers import health, auth
+from controllers import health, auth, games
 
 
 def apply_routes(app):
     app.add_url_rule('/', view_func=health.index, methods=['GET'], strict_slashes=False)
     # Auth
     app.add_url_rule('/auth/login', view_func=auth.login, methods=['POST'], strict_slashes=False)
+    # Games
+    app.add_url_rule('/game/create', view_func=games.save, methods=['POST'], strict_slashes=False)
