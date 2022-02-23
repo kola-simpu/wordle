130610af70fd9ebe1d708f4845db0f1afa503b1c
kolawolebalogun <kola@simpu.co>
2022-02-23 17:10:30 -0500

    Game CRUD

--- controllers/games.py
+++ controllers/games.py
@@ -11,10 +11,9 @@ def save():
     uuid = decoded_token.get('payload_sub')
     request_body = request.get_json()
     if uuid:
-        logging.debug(request.method)
         if request.method == 'POST':
             game_obj = {"name": request_body.get("name"), "word_id": request_body.get("word_id")}
-            logging.debug(game_obj)
+            return jsonify(game_obj), 200
         if request.method == 'PATCH':
             pass
         if request.method == 'DELETE':
