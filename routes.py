cfa62f6d8e0a75dd5b1d24d96a78913013165644
kolawolebalogun <kola@simpu.co>
2022-02-23 15:56:24 -0500

    JWT Authentication

--- routes.py
+++ routes.py
@@ -4,4 +4,4 @@ from controllers import health, auth
 def apply_routes(app):
     app.add_url_rule('/', view_func=health.index, methods=['GET'], strict_slashes=False)
     # Auth
-    app.add_url_rule('/auth/login', view_func=auth.login, methods=['GET'], strict_slashes=False)
+    app.add_url_rule('/auth/login', view_func=auth.login, methods=['POST'], strict_slashes=False)
